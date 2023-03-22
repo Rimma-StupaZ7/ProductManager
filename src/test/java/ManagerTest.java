a0a67030eb1a78cfca029e39929c0cc2594899c5
rimma test <valentin.rimma@gmail.com>
2023-03-22 14:50:58 +0400

    add new project

---
+++ src/test/java/ManagerTest.java
@@ -0,0 +1,89 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ManagerTest {
+
+    @Test
+    public void testFindProduct() {
+        Repository repo = new Repository();
+        Manager manager = new Manager(repo);
+        Book book1 = new Book(1, "Andromeda strain", 200, "Michael Crichton");
+        Book book2 = new Book( 13, "Shadow forest", 150, "Frank Tillier");
+        Book book3 = new Book(2, "Andromeda star", 230, "Maks Krypton");
+        Smartphone phone1 = new Smartphone(33, "samsung GalaxyA23",  25_000, "Samsung");
+
+        manager.add(book1);
+        manager.add(book2);
+        manager.add(book3);
+        manager.add(phone1);
+        Product[] actual = manager.searchBy("Andromeda");
+        Product[] expected = {book1, book3};
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void testFindZeroProduct() {
+        Repository repo = new Repository();
+        Manager manager = new Manager(repo);
+        Book book1 = new Book(1, "Andromeda strain", 200, "Michael Crichton");
+        Book book2 = new Book( 13, "Shadow forest", 150, "Frank Tillier");
+        Book book3 = new Book(2, "Andromeda star", 230, "Maks Krypton");
+        Book book4 = new Book( 5, "Amnesia",  199, "Frederico Aksat");
+        Smartphone phone1 = new Smartphone(33, "samsung GalaxyA23",  25_000, "Samsung");
+        Smartphone phone2 = new Smartphone(54, "NokiaX10", 25_600, "Nokia");
+
+        manager.add(book1);
+        manager.add(book2);
+        manager.add(book3);
+        manager.add(book4);
+        manager.add(phone1);
+        Product[] actual = manager.searchBy("Potter");
+        Product[] expected = {};
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void testFindPhoneProduct() {
+        Repository repo = new Repository();
+        Manager manager = new Manager(repo);
+        Book book1 = new Book(1, "Andromeda strain", 200, "Michael Crichton");
+        Book book2 = new Book( 13, "Shadow forest", 150, "Frank Tillier");
+        Book book3 = new Book(2, "Andromeda star", 230, "Maks Krypton");
+        Book book4 = new Book( 5, "Amnesia",  199, "Frederico Aksat");
+        Smartphone phone1 = new Smartphone(33, "samsung GalaxyA23",  25_000, "Samsung");
+        Smartphone phone2 = new Smartphone(54, "NokiaX10", 25_600, "Nokia");
+        Smartphone phone3 = new Smartphone(3, "NokiaX", 15_600, "Nokia");
+
+        manager.add(book1);
+        manager.add(book2);
+        manager.add(book3);
+        manager.add(phone1);
+        manager.add(phone2);
+        manager.add(phone3);
+
+        Product[] actual = manager.searchBy("Nokia");
+        Product[] expected = {phone2, phone3};
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void testFindProductBook() {
+        Repository repo = new Repository();
+        Manager manager = new Manager(repo);
+        Book book1 = new Book(1, "Andromeda strain", 200, "Michael Crichton");
+        Book book2 = new Book( 13, "Shadow forest", 150, "Frank Tillier");
+        Smartphone phone1 = new Smartphone(33, "samsung GalaxyA23",  25_000, "Samsung");
+        Smartphone phone2 = new Smartphone(54, "NokiaX10", 25_600, "Nokia");
+
+        manager.add(book1);
+        manager.add(book2);
+        manager.add(phone1);
+        manager.add(phone2);
+
+        Product[] actual = manager.searchBy("Shadow");
+        Product[] expected = {book2};
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+}
---
+++ src/test/java/RepositoryTest.java
@@ -0,0 +1,52 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RepositoryTest {
+    @Test
+    public void testRemoveProduct() {
+        Repository repo = new Repository();
+        Book book1 = new Book(1, "Andromeda strain", 200, "Michael Crichton");
+        Book book2 = new Book( 13, "Shadow forest", 150, "Frank Tillier");
+        Book book3 = new Book(2, "Andromeda star", 230, "Maks Krypton");
+        Book book4 = new Book( 5, "Amnesia",  199, "Frederico Aksat");
+        Smartphone phone1 = new Smartphone(33, "samsung GalaxyA23",  25_000, "Samsung");
+        Smartphone phone2 = new Smartphone(54, "NokiaX10", 25_600, "Nokia");
+
+
+        repo.add(book1);
+        repo.add(book2);
+        repo.add(book3);
+        repo.add(book4);
+        repo.add(phone1);
+        repo.add(phone2);
+        repo.removeById(1);
+        Product[] actual = repo.findAll();
+        Product[] expected = {book2, book3, book4, phone1, phone2};
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void testRemoveProductOnePhone() {
+        Repository repo = new Repository();
+        Book book1 = new Book(1, "Andromeda strain", 200, "Michael Crichton");
+        Book book2 = new Book( 13, "Shadow forest", 150, "Frank Tillier");
+        Book book3 = new Book(2, "Andromeda star", 230, "Maks Krypton");
+        Book book4 = new Book( 5, "Amnesia",  199, "Frederico Aksat");
+        Smartphone phone1 = new Smartphone(33, "samsung GalaxyA23",  25_000, "Samsung");
+        Smartphone phone2 = new Smartphone(54, "NokiaX10", 25_600, "Nokia");
+
+
+        repo.add(book1);
+        repo.add(book2);
+        repo.add(book3);
+        repo.add(book4);
+        repo.add(phone1);
+        repo.add(phone2);
+        repo.removeById(33);
+        Product[] actual = repo.findAll();
+        Product[] expected = {book1, book2, book3, book4, phone2};
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+}
